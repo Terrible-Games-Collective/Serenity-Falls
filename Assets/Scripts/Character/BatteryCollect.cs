7c3b36501c1646c9a8d268576a87cc3eb52eec43
Champin7 <Champin@users.noreply.github.com>
2019-09-25 12:09:40 -0600

    error in first commit adding assets

---
+++ Assets/Scripts/Character/BatteryCollect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryCollect : MonoBehaviour
+{
+    public GameObject battery;
+    public GameObject light;
+    public float scaleAmount = 5;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Battery"))
+        {
+            Debug.Log("Got here");
+            light.transform.localScale += new Vector3(scaleAmount, 0, 0);
+            Destroy(battery, 0);
+        }
+    }
+}
