ea0bee913f8d074120080d1082a9660a118c6184
Tannner Skomar <tdskomar@gmail.com>
2019-09-26 19:23:48 -0600

    Started Patrol AI
    Started messing around with the patroller AI in the newly created Patrol AI Testing Scene

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+
+    private int destinationSpot;
+    private float waitTime;
+
+    public float moveSpeed;
+    public float startWaitTime;
+    public Transform[] moveSpots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        destinationSpot = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[destinationSpot].position, moveSpeed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, moveSpots[destinationSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                if (destinationSpot+1 == moveSpots.Length)
+                {
+                    destinationSpot = -1;
+                }
+                destinationSpot++;
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+    }
+}
