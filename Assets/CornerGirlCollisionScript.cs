18ac7f77fbef4e9dfba0f82fd2ff7d285489870d
ChrysStr <36186243+ChrysStr@users.noreply.github.com>
2019-10-30 11:37:28 -0600

    Add files via upload
    +added to correct folder

---
+++ Assets/CornerGirlCollisionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerGirlCollisionScript : MonoBehaviour
+{
+    public GameObject myCornerGirl;
+    private CornerGirlAI _cornerGirlAIScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get variable
+        _cornerGirlAIScript = myCornerGirl.GetComponent<CornerGirlAI>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "character")
+        {
+            Detection();
+        }
+    }
+
+    public void Detection()
+    {
+        _cornerGirlAIScript.detected = true;
+    }
+}
