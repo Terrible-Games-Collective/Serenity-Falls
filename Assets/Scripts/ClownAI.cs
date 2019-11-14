182bf5808c4cc80e62749a9cf0d035e67e51c439
ChrysStr <36186243+ChrysStr@users.noreply.github.com>
2019-11-14 03:09:50 -0600

    Clown Mini-Monster AI Scripting
    + Proto Implimentation of Clown Mini Monster AI Scripting

---
+++ Assets/Scripts/ClownAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownAI : MonoBehaviour
+{
+    public bool detected = false;
+    public Transform clownTrans;
+
+    public GameObject clownScare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clownTrans = this.GetComponent<Transform>();
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+           detection();
+        }
+    }
+
+    public void detection()
+    {
+        detected = true;
+    }
+
+    public void jumpscare()
+    {
+        GameObject scare = Instantiate(clownScare, transform.position, Quaternion.identity);
+        Destroy(scare, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(detected)
+        {
+            jumpscare();
+        }
+    }
+}
