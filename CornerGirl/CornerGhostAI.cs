394dfcd9ed19586a8494863f5082ed18b5dfd429
ChrysStr <36186243+ChrysStr@users.noreply.github.com>
2019-11-20 01:43:17 -0600

    Updates for Mini-monsters
    +clown AI changes
    +cornerGhost AI changes

---
+++ CornerGirl/CornerGhostAI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerGhostAI : MonoBehaviour
+{
+    public bool detected = false;
+    public bool alreadyDetected = false;
+    public float moveSpeed = 20f;
+
+    public Rigidbody2D ghostRb;
+    public Transform ghostTrans;
+
+    public GameObject scareEffect;
+    public GameObject target;
+    public Vector3 spawnLoc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ghostRb = this.GetComponent<Rigidbody2D>();
+        ghostTrans = this.GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            detection();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            unDetected();
+        }
+    }
+
+    public void detection()
+    {
+        detected = true;
+        if(alreadyDetected)
+        {
+            jumpscare();
+        }
+    }
+
+    public void unDetected()
+    {
+        detected = false;
+        alreadyDetected = true;
+    }
+
+    public void escape()
+    {
+        ghostRb.AddForce(ghostTrans.right * moveSpeed, ForceMode2D.Impulse);
+    }
+
+    public void jumpscare()
+    {
+        Vector3 spawnloc = target.transform.position;
+        GameObject scare = Instantiate(scareEffect, transform.position, Quaternion.identity);
+        Destroy(scare, 5f);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnLoc = target.transform.position;
+        if (!alreadyDetected)
+        {
+            if (detected)
+            {
+                escape();
+            }
+        }else if(alreadyDetected)
+        {
+        
+
+        }
+    }
+}
---
+++ CornerGirl/jumpscareFollowPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpscareFollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 moves;
+    public float moveSpeed = 20f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        Debug.Log(direction);
+        //direction.Normalize();
+        //moves = direction;
+    }
+    /**
+    private void FixedUpdate()
+    {
+        moveCharacter(moves);
+    }
+    **/
+
+    /**
+    void moveCharacter(Vector2 movement)
+    {
+        rb.MovePosition((Vector2)transform.position + (movement * moveSpeed * Time.deltaTime));
+    }
+    **/
+}
