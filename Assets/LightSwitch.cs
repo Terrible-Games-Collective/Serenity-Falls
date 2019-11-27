ed1bd089c29bb9fd4b316e6029f674806eaacccf
Champin7 <Champin@users.noreply.github.com>
2019-11-27 16:33:27 -0600

    monster now turns off lihghts
    when the monster walks by lights it turns them off

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public float MaxRadius;
+
+    public void turnOffLights(Transform MainEnemy, float MaxRadius)
+    {
+        //Grab all objects that overlap with the FOV Radius
+        //if there is more then 100 items this could cause overflow
+        Collider2D[] withinRadius = new Collider2D[100];
+        int count = Physics2D.OverlapCircleNonAlloc(MainEnemy.position, MaxRadius, withinRadius);
+
+        for (int i = 0; i <= count; i++)
+        {
+            if (withinRadius[i] != null && withinRadius[i].transform.gameObject.CompareTag("MapLighting"))
+            {
+                withinRadius[i].transform.gameObject.SetActive(false);
+            }
+        }
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnOffLights(transform, MaxRadius);
+    }
+}
