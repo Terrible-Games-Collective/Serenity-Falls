50f0e9481737af4bc2c2cb4cd54da2454ed1a840
AeJe <amp183@mail.usask.ca>
2019-12-01 16:16:51 -0600

    UI update
    Updated the UI - now easy to see.
    Made Area of unamed rooms just be rooms

--- Assets/Scripts/Interactable/UI/PopUps.cs
+++ Assets/Scripts/Interactable/UI/PopUps.cs
@@ -2,36 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class PopUps : MonoBehaviour
 {
     public GameObject popupUI;
+    public string msg;
 
-    private float cooldownTimer;
-    private float cooldown;
-    // Start is called before the first frame update
+    // popup for player help - controls objectives...
     void Start()
     {
-        cooldownTimer = 3;
+        popupUI.SetActive(false);
+    }
+
+    public void closePopup()
+    {
+        popupUI.SetActive(false);
+    }
+    public void openPopup()
+    {
+        popupUI.GetComponent<Text>().text = msg;
         popupUI.SetActive(true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        CooldownTimer(popupUI);
-    }
-
-    public void CooldownTimer(GameObject uiObjects)
-    {
-        if (cooldownTimer > 0)
-        {
-            cooldownTimer -= Time.deltaTime;
-        }
-        if (cooldownTimer < 0)
-        {
-            cooldownTimer = 0;
-            uiObjects.SetActive(false);
-        }
     }
 }
